8a64217bb652e43a541c818bd166421346cea498
Vishal Kumar <javacheartofmine@gmail.com>
2025-07-28 23:41:59 +0530

    db script changes

--- src/common/database/typeorm-config/typeorm-config.service.ts
+++ src/common/database/typeorm-config/typeorm-config.service.ts
@@ -27,7 +27,7 @@ export class TypeormConfigService implements TypeOrmOptionsFactory {
       // password: databaseConfig.password,
       // database: databaseConfig.database,
       type: 'mysql',
-      host: 'localhost',
+      host: '82.112.234.241',
       port: 3306,
       username: 'codemerituser',
       password: 'GwjU067FL8hcmjQkXjaM',
@@ -36,7 +36,7 @@ export class TypeormConfigService implements TypeOrmOptionsFactory {
       // entities: [__dirname + '/../**/*.entity.{ts,js}'],
 
       // entities: ['src/**/*.entity.ts'],
-      synchronize: false,
+      synchronize: true,
     } as TypeOrmModuleOptions;
   }
 }
