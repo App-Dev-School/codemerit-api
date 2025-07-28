88a9aa681718e49e482b2f4b257ec0178d8e26a3
Vishal Kumar <javacheartofmine@gmail.com>
2025-07-29 00:29:05 +0530

    no db sync flag

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeormConfigService } from './typeorm-config/typeorm-config.service';
-
+const isDbDisabled = process.env.DISABLE_DB === 'true';
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
+    // Conditionally import TypeOrmModule based on environment variable
+    // TypeOrmModule.forRootAsync({
+    //   useClass: TypeormConfigService,
+    // }),
+     ...(!isDbDisabled
+      ? [TypeOrmModule.forRootAsync({
       useClass: TypeormConfigService,
-    }),
-  ],
+    })] : [])
+  ]
 })
 export class DatabaseModule {}
